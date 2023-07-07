a45ef1b085f247a0ee67cfc629a581f8ea7fbeb3
MOHD RAVISH <mohdravish2003@gmail.com>
2023-07-08 01:58:59 +0530

    Delete Request Done

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import Axios from "axios";
 
 function App() {
+
   const [addSection, setAddSection] = useState(false)
   const [formData, setFormData] = useState({
     name: "",
@@ -22,84 +23,82 @@ function App() {
     })
   }
 
-  // const fetchData = async() => {
-  //   axios.get("http://localhost:4500/")
-  // }
-
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    Axios.post("http://localhost:4500/create", formData)
-    // fetchData();
-    // console.log(data)
+  // Submit Data
+  const handleSubmit = async () => {
+    Axios.post("http://localhost:4500/save", formData)
     setAddSection(false)
+    window.location.reload();
   }
 
+  // Show Data
   useEffect(() => {
-    Axios.get("http://localhost:4500/").then((Response) => {
-      setDataList(Response.data)
-    });
+    Axios.get("http://localhost:4500/get").then((res) => {
+      setDataList(res.data)
+    })
+      .catch((err) => {
+        console.log(err)
+      })
   }, []);
 
-  const handleDelete = async(id)=>{
-   const data = await Axios.delete("http://localhost:4500/delete/"+id);
-    // alert(data)
-    // fetchData();
+  // Delete Data
+  const handleDelete = async (id) => {
+    Axios.delete("http://localhost:4500/delete/" + id);
+    window.location.reload();
   }
 
   return (
     <div className="container">
-      <button className='btn btn-add' onClick={() => setAddSection(true)}>ADD</button>
-      {
-        addSection && (
-          <div className="addContainer">
-            <form onSubmit={handleSubmit}>
-              <div className="close-btn" onClick={() => setAddSection(false)}><MdClose /></div>
-              <label htmlFor="name">Name : </label>
-              <input type="text" id="name" name="name" onChange={handleChange} />
+    <header>
+        <img src="Assets/user.png" alt="logo"></img>
+        <h1>User Hub<button onClick={() => setAddSection(true)}>ADD USER</button> </h1>
+      </header>
+      {addSection ? (
+        <div className="addContainer">
+          <form onSubmit={handleSubmit}>
+            <div className="close-btn" onClick={() => setAddSection(false)}><MdClose /></div>
+            <label htmlFor="name">Name : </label>
+            <input type="text" id="name" name="name" autoComplete='off' onChange={handleChange} required/>
 
-              <label htmlFor="name">Email : </label>
-              <input type="email" id="email" name="email" onChange={handleChange} />
+            <label htmlFor="name">Email : </label>
+            <input type="email" id="email" name="email" onChange={handleChange} required/>
 
-              <label htmlFor="mobile">Mobile : </label>
-              <input type="number" id="mobile" name="mobile" onChange={handleChange} />
+            <label htmlFor="mobile">Mobile : </label>
+            <input type="number" id="mobile" name="mobile" autoComplete='off' onChange={handleChange} required/>
 
-              <button className="btn">Submit</button>
-            </form>
-          </div>
-        )
-      }
-
-      <div className='tableContainer'>
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Mobile</th>
-              <th>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              dataList.map((dataItem) => {
-                return (
-                  <tr>
-                    <td>{dataItem.name}</td>
-                    <td>{dataItem.email}</td>
-                    <td>{dataItem.mobile}</td>
-                    <td>
-                      <button className='btn btn-edit'>Edit</button>
-                      <button className='btn btn-delete' onClick={()=>{handleDelete(dataItem.id)}}>Delete</button>
-                    </td>
-                  </tr>
-                )
-              })
-            }
-          </tbody>
-        </table>
-      </div>
-
+            <button className="btn">Submit</button>
+          </form>
+        </div>
+      ) : (
+        <div className='tableContainer'>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Mobile</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                dataList.map((dataItem) => {
+                  return (
+                    <tr>
+                      <td>{dataItem.name}</td>
+                      <td>{dataItem.email}</td>
+                      <td>{dataItem.mobile}</td>
+                      <td>
+                        <button className='btn btn-edit'>Edit</button>
+                        <button className='btn btn-delete' onClick={() => { handleDelete(dataItem._id) }}>Delete</button>
+                      </td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -3,38 +3,77 @@ const cors = require("cors")
 const mongoose = require("mongoose")
 const app = express()
 
-const routes = require("./routes/routes.js")
-
-require("dotenv").config()
-
-app.use(routes)
 app.use(cors())
 app.use(express.json())
 
+require("dotenv").config()
+
+// Port
 const PORT = process.env.PORT | 4500
 
-app.listen(PORT, () => console.log("SERVER IS RUNNING ON PORT 4500"))
-
-//Connection
+// Connection
 mongoose.connect("mongodb://127.0.0.1:27017/usermanagement")
 .then(()=>{
     console.log("MongoDB Connected")
 })
 .catch((err)=>console.log(err))
 
-//Read
-// app.get("/", async(req, res) => {
-//     const data = await userModel.find({})
-//     res.send(data)
-// })
+// Schema
+const schemaData = new mongoose.Schema({
+    name: String,
+    email: String,
+    mobile: String,
+},{
+    timeStamps : true
+});
 
-//Create
-// app.post("/create", async(req, res)=>{
-//     console.log(req.body)
-//     const data = new userModel(req.body)
-//     await data.save()
-//     res.send({success : true, message : "Data Saved Successfully", data : data})
-// })
+// Model
+const userModel = mongoose.model("User", schemaData);
+
+// Create
+app.post("/save", async(req, res)=>{
+    userModel.create(req.body)
+    .then((data) => {
+        console.log("Data Submitted!")
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
+// Read
+app.get("/get", async(req, res) => {
+    const data = await userModel.find({})
+    res.send(data)
+})
+
+// Delete
+app.delete("/delete/:id", async(req, res) => {
+    const {id} = req.params
+    userModel.findByIdAndDelete(id)
+    .then(() => {
+        console.log("Data Deleted!")
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
+// Server
+app.listen(PORT, () => console.log(`SERVER IS RUNNING ON PORT ${PORT}`))
+
+//Update
+// module.exports.updateUsers = async(req, res) => {
+//     const {id} = req.params
+//     const {task} = req.body
+//     userModel.findByIdAndUpdate(id, {task})
+//     .then(() => {
+//         console.log("Updated Successfully!")
+//     })
+//     .catch((err) => {
+//         console.log(err)
+//     })
+// }
 
 //Update
 // app.put("/update", async(req, res)=>{
@@ -43,11 +82,4 @@ mongoose.connect("mongodb://127.0.0.1:27017/usermanagement")
 //     console.log(rest)
 //     const data = await userModel.updateOne({_id : id}, rest)
 //     res.send({success : true, message : "Data Updated Successfully", data : data})
-// })
-
-// Delete
-// app.delete("/delete/:id", async(req, res)=>{
-//     const id = req.params.id
-//     userModel.deleteOne({_id : id})
-//     // res.send({success : true, message : "Data Deleted Successfully", data : data})
 // })
