1a12787c6987c86783739b62e371c6d474b6d681
MOHD RAVISH <mohdravish2003@gmail.com>
2023-07-08 13:36:01 +0530

    Toastify Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { MdClose } from "react-icons/md";
 import { useEffect, useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Axios from "axios";
 
 function App() {
@@ -26,44 +28,58 @@ function App() {
   // Submit Data
   const handleSubmit = async () => {
     Axios.post("http://localhost:4500/save", formData)
+      .then(fetchData())
+      .then(toast.success("Data Submitted!", {
+        position: toast.POSITION.TOP_CENTER
+      }))
     setAddSection(false)
-    window.location.reload();
+    // window.location.reload();
   }
 
-  // Show Data
-  useEffect(() => {
+  // Fetch Data from api
+  const fetchData = () => {
     Axios.get("http://localhost:4500/get").then((res) => {
       setDataList(res.data)
     })
       .catch((err) => {
         console.log(err)
       })
+  }
+
+  // Show Data
+  useEffect(() => {
+    fetchData()
   }, []);
 
   // Delete Data
   const handleDelete = async (id) => {
-    Axios.delete("http://localhost:4500/delete/" + id);
-    window.location.reload();
+    Axios.delete("http://localhost:4500/delete/" + id)
+      .then(fetchData())
+      .then(toast.success("Data Deleted!", {
+        position: toast.POSITION.TOP_CENTER
+      }))
+    // window.location.reload();
   }
 
   return (
     <div className="container">
-    <header>
+      <header>
         <img src="Assets/user.png" alt="logo"></img>
         <h1>User Hub<button onClick={() => setAddSection(true)}>ADD USER</button> </h1>
       </header>
+      <ToastContainer />
       {addSection ? (
         <div className="addContainer">
           <form onSubmit={handleSubmit}>
             <div className="close-btn" onClick={() => setAddSection(false)}><MdClose /></div>
             <label htmlFor="name">Name : </label>
-            <input type="text" id="name" name="name" autoComplete='off' onChange={handleChange} required/>
+            <input type="text" id="name" name="name" autoComplete='off' onChange={handleChange} required />
 
             <label htmlFor="name">Email : </label>
-            <input type="email" id="email" name="email" onChange={handleChange} required/>
+            <input type="email" id="email" name="email" onChange={handleChange} required />
 
             <label htmlFor="mobile">Mobile : </label>
-            <input type="number" id="mobile" name="mobile" autoComplete='off' onChange={handleChange} required/>
+            <input type="number" id="mobile" name="mobile" autoComplete='off' onChange={handleChange} required />
 
             <button className="btn">Submit</button>
           </form>
@@ -80,7 +96,7 @@ function App() {
               </tr>
             </thead>
             <tbody>
-              {
+              {dataList[0] ? (
                 dataList.map((dataItem) => {
                   return (
                     <tr>
@@ -89,12 +105,17 @@ function App() {
                       <td>{dataItem.mobile}</td>
                       <td>
                         <button className='btn btn-edit'>Edit</button>
-                        <button className='btn btn-delete' onClick={() => { handleDelete(dataItem._id) }}>Delete</button>
+                        <button className='btn btn-delete'
+                          onClick={() => { handleDelete(dataItem._id) }}>Delete</button>
                       </td>
                     </tr>
                   )
                 })
-              }
+              ) : (
+                <div className='no-data'>
+                  <p>No Data Found!</p>
+                </div>
+              )}
             </tbody>
           </table>
         </div>
